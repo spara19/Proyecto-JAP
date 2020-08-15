93da047b2d0583e739e28fa24b359e9ebbb239bc
SPA19 <68876376+SPA19@users.noreply.github.com>
2020-08-14 22:52:15 -0300

    Funciona?
    Viendo la mier... del redirect con el submit

--- js/login.js
+++ js/login.js
@@ -1,25 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+// document.addEventListener("DOMContentLoaded", function(e){
 
-});
+// });
 
 document.getElementById("SubmitButton").addEventListener("click", DatosUsuario); 
+// document.getElementById("SubmitButton").addEventListener("click", GoToIndex); //
+
+
+// function GoToIndex() {
+//     if (sessionStorage.getItem("Email") != null && document.getElementById("inputEmail").checkValidity()) {
+//         //window.location.href ='index.html';
+        
+//     }
+// }
+
 
 function DatosUsuario() {
-var usuario = document.getElementById("inputEmail").value;
-var contra = document.getElementById("inputPassword").value;
-sessionStorage.setItem("Email", usuario);
-sessionStorage.setItem("Contraseña", contra);
-alert(sessionStorage.getItem("Email"));
+    var usuario = document.getElementById("inputEmail").value;
+    var contra = document.getElementById("inputPassword").value;
+    sessionStorage.setItem("Email", usuario);
+    sessionStorage.setItem("Contraseña", contra);
+    //GoToIndex();
 }
 
-document.getElementById("SubmitButton").addEventListener("click", GoToIndex);
-
-function GoToIndex() {
-    alert(sessionStorage.getItem("Email"))
-    if (sessionStorage.getItem("Email")=== null) {
-        window.location.replace("www.google.com")
-    }
-    }
