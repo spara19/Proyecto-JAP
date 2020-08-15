e5f429e8f518a028826e31fde2fcd049de5983a5
SPA19 <68876376+SPA19@users.noreply.github.com>
2020-08-15 01:02:22 -0300

    NO TE BORRES MAS

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function gotoLogin() {
+    if (sessionStorage.getItem("Email") === null && sessionStorage.getItem("Contraseña") === null) {
+        window.location.href = "login.html";
+    }
+}
+gotoLogin();
+
+
+    
+// if (sessionStorage.getItem("Email") === null && sessionStorage.getItem("Contraseña") === null) {
+ //   window.location.href = "login.html";
+
+
