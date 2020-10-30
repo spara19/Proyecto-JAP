b2674a80da69b97e5eedea218cae0ddc37f0a4be
SPA19 <68876376+SPA19@users.noreply.github.com>
2020-10-30 20:06:37 -0300

    Entrega 7a
    Guardar multiples usuarios, con informacion particual de cada uno

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,5 +2,107 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    unique_user = sessionStorage.getItem("Email")
+    if (localStorage.getItem(unique_user) === "complete") {
+        showProfileInfo();
+    }
+
 
 });
+
+function checkNewProfileValidation() {     // Valida que los cambios sean correctos.
+    parentID = document.getElementById("new-profile-info-form");
+    childIDs = parentID.querySelectorAll('*[id]');
+    let local_validation = 1
+    for (var i = 0, len = childIDs.length; i < len; ++i) {
+        let input = childIDs[i]
+        if (i != 1) { // Ignora validación opcional de segundo nombre.
+            if (input.value.trim() === '') {
+                input.className = "form-control input_error";  
+                local_validation = 0  
+            } else {
+                input.className = "form-control input_success"; 
+            }
+        }
+    }
+    if (local_validation === 1) {
+        saveNewProfile();
+        showProfileInfo();
+    }
+}
+
+function getModalIDS() { // Toma el nodo de IDs del modal (son todos inputs)
+    parentID = document.getElementById("new-profile-info-form");
+    childIDs = parentID.querySelectorAll('*[id]');
+    return childIDs
+}
+
+function getModalInputsValues() { // Toma el valor de cada input del modal
+    childIDs = getModalIDS()
+    var inputs_values = []    
+    for (var i = 0, len = childIDs.length; i < len; ++i) {
+        let input = childIDs[i].value
+        inputs_values.push(input)     
+    }
+    return inputs_values
+}
+
+function saveNewProfile() {   // Guarda, como objeto, los valores de los input. Luego como string para guardar en localStorage
+
+    if (localStorage.getItem("profile_info_string") != null) {
+        profile_info = JSON.parse(localStorage.getItem("profile_info_string"));
+    } else {
+        profile_info = {}
+    }
+    inputs_values = getModalInputsValues();
+    unique_user = sessionStorage.getItem("Email");
+    profile_info_user = {first_name: inputs_values[0], second_name: inputs_values[1], first_surname: inputs_values[2], second_surname: inputs_values[3], phone: inputs_values[4], email: inputs_values[5], age: inputs_values[6]};
+    profile_info[unique_user] = profile_info_user;
+    profile_info_string = JSON.stringify(profile_info);
+    localStorage.setItem("profile_info_string", profile_info_string);
+    localStorage.setItem(unique_user, "complete");
+
+}
+
+function getProfileDivsIDS() {  // Toma los ID de los div donde se muestra la info del usuario.
+    names = document.getElementById("profile-names"); 
+    surnames = document.getElementById("profile-surnames");
+    phone = document.getElementById("profile-phone");
+    email = document.getElementById("profile-email");
+    age = document.getElementById("profile-age");
+    inputs = [names, surnames, phone, email, age];
+    return inputs;
+}
+
+function showProfileInfo() {      // Sobreescribe los div donde se muestra info de usuario, con nueva info.
+    profile_inputs = getProfileDivsIDS();
+    unique_user = sessionStorage.getItem("Email");
+    new_profile_info_user = JSON.parse(localStorage.getItem("profile_info_string"));
+    new_profile_info = new_profile_info_user[unique_user];
+
+    profile_inputs[0].innerHTML = new_profile_info.first_name + " " + new_profile_info.second_name;
+    profile_inputs[1].innerHTML = new_profile_info.first_surname + " " + new_profile_info.second_surname;        
+    profile_inputs[2].innerHTML = new_profile_info.phone;   
+    profile_inputs[3].innerHTML = new_profile_info.email;
+    profile_inputs[4].innerHTML = new_profile_info.age;
+
+    
+}
+
+function loadActualProfile() {   // Hace que al abrir el modal, los inputs por default, muestren la info ya existente.
+    unique_mail = sessionStorage.getItem("Email");
+    if (localStorage.getItem(unique_mail) === "complete") {
+        parentID = document.getElementById("new-profile-info-form");
+        modal_inputs = parentID.querySelectorAll('*[id]');
+        default_modal_inputs_users = JSON.parse(localStorage.getItem("profile_info_string"));
+        default_modal_inputs = default_modal_inputs_users[unique_user];
+            modal_inputs[0].value = default_modal_inputs.first_name;    
+            modal_inputs[1].value = default_modal_inputs.second_name;    
+            modal_inputs[2].value = default_modal_inputs.first_surname;
+            modal_inputs[3].value = default_modal_inputs.second_surname;
+            modal_inputs[4].value = default_modal_inputs.phone;
+            modal_inputs[5].value = default_modal_inputs.email;
+            modal_inputs[6].value = default_modal_inputs.age;  
+    } 
+}
+
