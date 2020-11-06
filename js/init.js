e5399efefec08bb7d829cbb737051a7c28769682
SPA19 <68876376+SPA19@users.noreply.github.com>
2020-11-06 20:54:45 -0300

    7.5

--- js/init.js
+++ js/init.js
@@ -56,19 +56,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 
   ///////////////
 /////// Nombre de usuario //////
-if (window.location.href.split("/").pop() !== "login.html") {
-  document.getElementById("nombreMail").innerHTML +=  `<div class="btn-group">
-  <button type="button" class="btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${(sessionStorage.getItem("Email"))}</button>
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="cart.html">Mi carrito</a>
-    <a class="dropdown-item" href="my-profile.html">Mi perfil</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" id="cerrarSesion">Cerrar sesión</a>
-  </div>
-</div>`
-}
-
+changeNavBar();
 });
 /// Fijar como se ejecuta la funcion de cerrar sesion, el onload sirve para que pueda tomar la ID fijada con JS /// 
 onload = function() {
@@ -78,12 +66,85 @@ onload = function() {
 }
 
 //// Funcion de cerrar sesion, elimina los datos guardados en los sessionStorage y manda al login ///
+function displayUserMenu() {
+  if (window.location.href.split("/").pop() !== "login.html") {
+    document.getElementById("nombreMail").innerHTML +=  `<div class="btn-group">
+        <button type="button" class="btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">${(sessionStorage.getItem("Email"))}</button>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="cart.html">Mi carrito</a>
+          <a class="dropdown-item" href="my-profile.html">Mi perfil</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" id="cerrarSesion">Cerrar sesión</a>
+        </div>
+      </div>`
+    }
+  }
+
 function closeSession() {
   var answer = confirm("¿Quiere cerrar sesión?");
   if (answer) {
-  sessionStorage.removeItem("Email");
-  sessionStorage.removeItem("Contraseña");
-  window.location.href = "login.html"
+    sessionStorage.removeItem("Email");
+    sessionStorage.removeItem("Contraseña");
+    window.location.href = "login.html"
+  }
 }
 
+
+function reportWindowSize() {
+  let widthOutput = window.innerWidth;
+  return widthOutput
+}
+
+window.addEventListener('resize', function() {
+  if (window.location.href.split("/").pop() !== "login.html") {
+    changeNavBar()
+  }
+})
+
+function changeNavBar() {
+  let windowsWidth = reportWindowSize();
+  if (windowsWidth < 768) {
+    smallNavBar();
+  } else if (windowsWidth >= 767){
+    normalNavBar();
+  }
+}
+
+function smallNavBar() {
+      var navBar = document.getElementsByTagName("nav");
+      navBar[0].className = "navbar navbar-expand-lg navbar-dark bg-dark site-header sticky-top py-1"
+      var newnavBar =   `
+      <a class="navbar-brand" href="index.html">Inicio</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav container d-flex flex-column flex-md-row justify-content-between">
+          <a class="nav-item nav-link" href="categories.html">Categorías <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="products.html">Productos</a>
+          <a class="nav-item nav-link" href="sell.html">Vender</a>
+          <a id="nombreMail" class="nav-item nav-link" href=""></a>
+        </div>
+      </div>
+      `
+      navBar[0].innerHTML = newnavBar
+      displayUserMenu()
+}
+
+function normalNavBar() {
+    var navBar = document.getElementsByTagName("nav");
+    navBar[0].className = "site-header sticky-top py-1 bg-dark"
+    var newnavBar = `
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+      <a class="py-2 d-none d-md-inline-block" href="index.html">Inicio</a>
+      <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
+      <a class="py-2 d-none d-md-inline-block" href="products.html">Productos</a>
+      <a class="py-2 d-none d-md-inline-block" href="sell.html">Vender</a>
+      <a id="nombreMail" class="py-2 d-none d-md-inline-block" href=""></a>
+    </div>
+    `
+
+    navBar[0].innerHTML = newnavBar
+    displayUserMenu()
 }
